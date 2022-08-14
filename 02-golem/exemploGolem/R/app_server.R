fc15b4957a9141abcd7867426178c0194332b8cb
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-08-14 16:14:59 -0300

    building the update of the poluente selectinput and the echart graphic

--- 02-golem/exemploGolem/R/app_server.R
+++ 02-golem/exemploGolem/R/app_server.R
@@ -1,10 +1,39 @@
 #' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
+#'
+#' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-app_server <- function( input, output, session ) {
-  # Your application server logic 
-  
+app_server <- function(input, output, session) {
+  # Your application server logic
+
+  observe({
+
+    # filtrar poluentes de acordo com a estação
+    opcoes <- cetesb |>
+      dplyr::filter(estacao_cetesb == input$estacao) |>
+      dplyr::pull(poluente) |>
+      unique()
+
+    # atualizar o select input com a nova lista
+    updateSelectInput(
+      inputId = "poluente",
+      choices = opcoes
+    )
+  })
+
+  output$grafico <- echarts4r::renderEcharts4r({
+    req(input$poluente) # precisa da escolha do poluente pra rodar
+
+    cetesb |>
+      dplyr::filter(
+        estacao_cetesb == input$estacao,
+        poluente == input$poluente
+      ) |>
+      dplyr::group_by(data) |>
+      dplyr::summarise(media = mean(concentracao, na.rm = TRUE)) |>
+      echarts4r::e_chart(x = data) |>
+      echarts4r::e_line(serie = media)
+    
+  })
 }
