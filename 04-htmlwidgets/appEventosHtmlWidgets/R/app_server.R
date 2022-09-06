acd5efab4f901672e87e04e0e2fa6933ab45028f
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-09-05 23:35:47 -0300

    building the ui of the plotu module and the plotly output

--- 04-htmlwidgets/appEventosHtmlWidgets/R/app_server.R
+++ 04-htmlwidgets/appEventosHtmlWidgets/R/app_server.R
@@ -8,5 +8,6 @@ app_server <- function(input, output, session) {
   # Your application server logic
   mod_reactable_server("reactable_1")
   mod_leaflet_server("leaflet_1")
+  mod_plotly_server("plotly_ui_1")
 
 }
--- 04-htmlwidgets/appEventosHtmlWidgets/R/mod_plotly.R
+++ 04-htmlwidgets/appEventosHtmlWidgets/R/mod_plotly.R
@@ -4,28 +4,53 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_plotly_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    bs4Dash::bs4Card(
+      title = "Filtro",
+      width = 12,
+      selectInput(
+        inputId = ns("ano"),
+        label = "Selecione um ano",
+        choices = sort(unique(pnud$ano)),
+        width = "25%" # para não ocupar toda a linha
+      )
+    ),
+    plotly::plotlyOutput(outputId = ns("grafico"))
   )
 }
-    
+
 #' plotly Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_plotly_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
+    output$grafico <- plotly::renderPlotly({
+      p <- pnud |>
+        dplyr::filter(ano == input$ano) |>
+        ggplot2::ggplot() +
+        ggplot2::aes(
+          x = rdpc,
+          y = espvida
+        ) +
+        ggplot2::geom_point()
+
+      plotly::ggplotly(p)
+
+    })
+
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_plotly_ui("plotly_ui_1")
-    
+
 ## To be copied in the server
 # mod_plotly_server("plotly_ui_1")
