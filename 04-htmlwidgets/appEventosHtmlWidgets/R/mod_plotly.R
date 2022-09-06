ba1418d41196b9c5397d9be98859d899c880629c
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-09-05 23:56:08 -0300

    add reactable output with point plotly filter to the data

--- 04-htmlwidgets/appEventosHtmlWidgets/R/mod_plotly.R
+++ 04-htmlwidgets/appEventosHtmlWidgets/R/mod_plotly.R
@@ -20,7 +20,16 @@ mod_plotly_ui <- function(id){
         width = "25%" # para não ocupar toda a linha
       )
     ),
-    plotly::plotlyOutput(outputId = ns("grafico"))
+    fluidRow(
+      column(
+        width = 8,
+        plotly::plotlyOutput(outputId = ns("grafico"))
+      ),
+      column(
+        width = 4,
+        reactable::reactableOutput(ns("tabela"))
+      )
+    )
   )
 }
 
@@ -31,9 +40,15 @@ mod_plotly_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    # criar base filtrada -----------------------------------------------------
+    pnud_filtrada <- reactive({
+      pnud |>
+        dplyr::filter(ano == input$ano)
+    })
+
+    # output gráfico plotly ---------------------------------------------------
     output$grafico <- plotly::renderPlotly({
-      p <- pnud |>
-        dplyr::filter(ano == input$ano) |>
+      p <- pnud_filtrada() |>
         ggplot2::ggplot() +
         ggplot2::aes(
           x = rdpc,
@@ -42,6 +57,49 @@ mod_plotly_server <- function(id){
         ggplot2::geom_point()
 
       plotly::ggplotly(p)
+
+    })
+
+
+    # output tabela -----------------------------------------------------------
+    output$tabela <- reactable::renderReactable({
+      # para verificar o que retorna quando os pontos dos gráfico são clicados
+      # print(plotly::event_data("plotly_click"))
+
+      # salvar o ponto que foi clicado
+      ponto_selecionado <- plotly::event_data("plotly_click")
+
+
+      # condicional para exibir a tabela somente se um ponto for selcionado
+      if (is.null(ponto_selecionado)) {
+        NULL
+      } else {
+        pnud_filtrada() |>
+          # cortar somente a linha que representa o número do ponto selecionado
+          dplyr::slice(ponto_selecionado$pointNumber) |>
+          dplyr::select(
+            muni_nm,
+            rdpc,
+            espvida,
+            idhm,
+            gini) |>
+          dplyr::mutate(# transformar todas as colunas em character para não ter problemas ao
+            # empihlar os dados na próxima etapa
+            dplyr::across(
+              .fns = as.character
+            )
+          ) |>
+          tidyr::pivot_longer(
+            cols = dplyr::everything(),
+            names_to = "var",
+            values_to = "val"
+          ) |>
+          reactable::reactable(# renomear colunas exibidas na tabela
+            columns = list(
+              var = reactable::colDef(name = ""),
+              val = reactable::colDef(name = "")
+            ))
+      }
 
     })
 
