3f71733072833a9682c6e89b791909c4b5ce9e5d
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-09-18 20:44:09 -0300

    add portuguese comments

--- outros/20220917_data_validator_package.R
+++ outros/20220917_data_validator_package.R
@@ -80,97 +80,129 @@ report |> data.validator::save_report(
 validator <- data.validator::data_validation_report()
 
 mtcars |> 
+    # iniciar pipeline de validação
     data.validator::validate() |>
+    
+    # checar se existe NAs dentro de algumas colunas
     data.validator::validate_cols(
         description = "No NA's inside mpg:carb columns",
         predicate = assertr::not_na,
         mpg:carb
     ) |>
+    
+    # checar se os valores são iguais a 0 ou 2 em duas colunas
     data.validator::validate_cols(
         description = "vs and am values equal 0 or 2 only", 
         predicate = assertr::in_set(c(0, 2)), 
         vs, 
         am
     ) |>
+    
+    # checar se os valores são iguais a 3 ou 4 em duas colunas
     data.validator::validate_cols(
         description = "vs and am values should equal 3 or 4",
         skip_chain_opts = TRUE,
-        error_fun = assertr::warning_append,
+        error_fun = assertr::warning_append, # warning (aviso apenas)
         predicate = assertr::in_set(c(3, 4)),
         gear,
         carb
     ) |>
+    
+    # checar se a soma das linhas de cada coluna detro de um range de colunas é 
+    # menor ou igual a 2
     data.validator::validate_rows(
         description = "Each row sum for am:vs columns is less or equal 2",
         rowSums, 
         assertr::within_bounds(0, 2), 
         vs:am
     ) |>
+    
+    # checar se a soma das linhas de cada coluna detro de um range de colunas é 
+    # menor ou igual a 1
     data.validator::validate_rows(
         description = "Each row sum for am:vs columns is less or equal 1",
         rowSums, 
         assertr::within_bounds(0, 1), 
         vs:am
     ) |>
+    
+    # checar uma coluna usando o desvio padrão dos dados
     data.validator::validate_cols(
         description = "For wt and qsec we have: abs(col) < 4 * sd(col)", 
         assertr::within_n_sds(4), 
         wt, 
         qsec
     ) |>
+    
+    # checar uma coluna usando o desvio padrão dos dados
     data.validator::validate_cols(
         description = "For wt and qsec we have: abs(col) < 2 * sd(col)", 
         assertr::within_n_sds(2), 
         wt, 
         qsec
     ) |>
+    
+    # validação estatística das linhas (distância da mediana)
     data.validator::validate_rows(
         description = "Using mpg:carb mahalanobis distance for each observation is within 30 median absolute deviations from the median",
         row_reduction_fn = assertr::maha_dist,
         assertr::within_n_mads(30),
         mpg:carb
     ) |>
+    
+    # validação estatística das linhas (distância da mediana)
     data.validator::validate_rows(
         description = "Using mpg:carb mahalanobis distance for each observation is within 3 median absolute deviations from the median",
         row_reduction_fn = assertr::maha_dist,
         assertr::within_n_mads(3),
         mpg:carb
     ) |>
+    
+    # checar se uma coluna possui apenas valores positivos
     data.validator::validate_if(
         description = "Column drat has only positive values", 
         drat > 0
     ) |>
+    
+    # checar se uma coluna possui apenas valores maiores que 3
     data.validator::validate_if(
         description = "Column drat has only values larger than 3", 
         drat > 3
     ) |>
+    
+    # adicionar resultados no objeto de validação
     data.validator::add_results(validator)
 
-
+# exibir relatório no console
 data.validator::get_results(validator)
 
-
+# exportar relatório
 validator |> data.validator::save_report(
     output_file = "20220918_validation_report_example_3.html",
     output_dir = "outros/"
 )
 
+# abrir relatório no navegador
 browseURL("outros/20220918_validation_report_example_3.html")
 
 
+# exportar relatório bruto
 validator |> data.validator::save_report(
     output_file = "20220918_validation_report_example_3_raw.html",
     ui_constructor = data.validator::render_raw_report_ui,
     output_dir = "outros/"
     )
 
+# abrir relatório no navegador
 browseURL("outros/20220918_validation_report_example_3_raw.html")
 
 
+# exportar relatório em csv
 validator |> data.validator::save_results(
     file_name = "outros/20220918_results_example_3.csv"
     )
 
+# exportar relatório em txt
 validator |> data.validator::save_summary(
     file_name = "outros/20220918_validation_log_example_3.txt"
     )
