1d59cd391ff2b7fcac05fc64f5e7f7c3a5acc8df
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-10-18 20:27:07 -0300

    changing the pack:load to make the app work in the cloud

--- 04-htmlwidgets/appEventosHtmlWidgets/app.R
+++ 04-htmlwidgets/appEventosHtmlWidgets/app.R
@@ -2,7 +2,11 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+# com esse estilo não funcionou no shinyio, tive que usar apenas 'load_all'
+# pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+
+# assim funcionou:
+pkgload::load_all()
 
 # é opcional, usado para controlar o que vai ser enviado pra nuvem ou nao
 # algo como: se essa opção estiver como TRUE, leva arquivo X ou não
